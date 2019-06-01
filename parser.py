a4d0d5c0b88f79db60b240de15ab14672de7d0aa
daboliang <steve.liang@f5tradingllc.com>
2019-05-31 21:14:04 -0500

    added __init__.py, CaseInsentiveDict class

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import re
+from CaseInsensitiveDict import CaseInsensitiveDict
 import pandas as pd
 
 line = "-Unit #203-B,  300-499,   North State STREET Boulevard Avenue @chicaog, ste 3210b"
@@ -18,9 +19,10 @@ line = line.strip()
 
 suffix_csv = pd.read_csv('./suffix.csv')
 suffix_dict = suffix_csv.set_index('input')['output'].to_dict()
+suffix_i = CaseInsensitiveDict(suffix_dict)
 
 def replace_it(m):
-    return suffix_dict.get(m.group('key'), m.group(0))
+    return suffix_i.get(m.group('key'), m.group(0))
 
 line = re.sub(r'(?P<key>[a-zA-Z]+)', replace_it, line)
 print(line)
